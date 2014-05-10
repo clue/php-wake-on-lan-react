72bd75188b0278ae0118fd097a1d0b071f32b9be
Christian Lück <christian@lueck.tv>
2014-05-11 00:38:03 +0200

    Add tests for Sender

---
+++ tests/SenderTest.php
@@ -0,0 +1,71 @@
+<?php
+
+use Clue\Wol\Sender;
+
+class SenderTest extends TestCase
+{
+    public function setUp()
+    {
+        $this->socket = $this->getMockBuilder('Socket\React\Datagram\Socket')
+                             ->disableOriginalConstructor()
+                             ->getMock();
+
+        $this->sender = new Sender($this->socket);
+    }
+
+    public function testSend()
+    {
+        $this->socket
+             ->expects($this->once())
+             ->method('send');
+
+        $this->sender->send('aa:bb:cc:dd:ee:ff');
+    }
+
+    /**
+     * @dataProvider provideValidAddress
+     * @param string $address
+     * @param string $mac
+     */
+    public function testValidAddress($address, $mac)
+    {
+        $this->assertEquals($mac, $this->sender->coerceMac($address));
+    }
+
+    public function provideValidAddress()
+    {
+        return array(
+            array(
+                'Aa:bB:12:EF:34:56',
+                'AA:BB:12:EF:34:56'
+            ),
+            array(
+                'Aa-bB-12-EF-34-56',
+                'AA:BB:12:EF:34:56'
+            ),
+            array(
+                'AabB12EF3456',
+                'AA:BB:12:EF:34:56'
+            ),
+        );
+    }
+
+    /**
+     * @dataProvider provideInvalidAddress
+     * @expectedException InvalidArgumentException
+     * @param unknown $address
+     */
+    public function testInvalidAddress($address)
+    {
+       $this->sender->coerceMac($address);
+    }
+
+    public function provideInvalidAddress($address)
+    {
+        return array(
+            array('aa:bb:cc'),
+            array('aa:aa:aa:aa:aa:aa:aa'),
+            array('gg:gg:gg:gg:gg:gg'),
+        );
+    }
+}
